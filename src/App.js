826fb8e22ea33ee7e030303fc995f1d1b610db8f
Siphiwe Manda <siphiwe.manda@finova.tech>
2022-02-13 10:20:45 +0000

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from "./Component/Navbar";
 
 function App() {
   return (
-
     <div className="App">
+      <Navbar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/Component/Navbar.js
@@ -0,0 +1,21 @@
+import {NavLink} from "react-router-dom";
+
+const Navbar = () => {
+    return (
+<div className="navbar">
+    <h1> Nyanja Fruit Growers</h1>
+    <ul>
+        <li>About us</li>
+        <li>Our Produce</li>
+        <li> Member Spotlight</li>
+        <li>Our Customers</li>
+        <li>Contact Us</li>
+        <li>Join Us</li>
+    </ul>
+</div>
+
+        );
+}
+
+
+export default Navbar;
