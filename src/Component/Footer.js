cc3eee2295d86c015b0fc84facee63cc3ba13e0f
siphiwe <siphiwe>
2022-06-29 21:19:38 +0100

    working on footer

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -1,20 +1,17 @@
+import twitter from '../IMG/twitter.png'
+import facebook from '../IMG/facebook.png'
+import instagram from '../IMG/instagram.png'
+import '../CSS/Footer.css'
 const Footer = () => {
     return(
-        <div className="ContactSection">
-            <div>
-                <h1>Join Us </h1>
-                <p>Are you a small scale fruit farmer interested in joining our association? Please contact us for further information.</p>
-                <a href="#" className="btn btn-white btn-animated">Email</a>
-            </div>
-            <div>
-                <h1>Contact Us</h1>
-                <p>To find out more about our association or to discuss investment opportunities, please contact our chairman at the address below.</p>
-                <a href="#" className="btn btn-white btn-animated">Email</a>
-            </div>
-            <div>
-                <h1> Place An Order</h1>
-                <p>Are you interested in our fresh or processed product? become an offtaker or place an order for your store here.</p>
-                <a href="#" className="btn btn-white btn-animated">Email</a>
+        <div className="Footer">
+            <div className="FooterTop">
+                <h4>Nyanja Fruit Growers</h4>
+                <ul>
+                    <img src={facebook}  width={24} height={24} alt="facebook logo"/>
+                    <img src={instagram}  width={24} height={24} alt="instgram logo"/>
+                    <img src={twitter}  width={24} height={24} alt="twitter logo"/>
+                </ul>
             </div>
         </div>
 
