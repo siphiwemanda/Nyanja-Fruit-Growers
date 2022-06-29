00104e866bcdc975a19f5e78d1ce4fa6940d3250
Siphiwe Manda <siphiwe.manda@finova.tech>
2022-06-29 18:07:35 +0100

    Update Navabar

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,12 +1,14 @@
 import mangoes from '../IMG/mangos.jpg'
+import '../CSS/Header.css'
 const Header = () => {
     return (
         <div className="Header">
             <div className="HeaderMainText">
                 <h1> Nyanja Fruit {"\n"}Growers</h1>
                 <p> A cooperative association of smallholder fruit growing farmers in Malawi.</p>
+                <li className="btn btn-animated">Contact Us/Join Us</li>
             </div>
-            <div className="FruitGrid">
+            <div className="HeaderImg">
                 <img src={mangoes}  width={200} height={200} alt="Fruit"/>
             </div>
 
--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,17 +1,16 @@
-
+import '../CSS/Navbar.css'
 const Navbar = () => {
     return (
 <div className="navbar">
- <ul>
-     <li className="navBarHeader">Nyanja Fruit Growers</li>
- </ul>
+
+     <h4 className="navBarHeader">Nyanja Fruit Growers</h4>
+
     <ul className="list">
         <li className="btn btn-animated">About us</li>
         <li className="btn btn-animated">Our Produce</li>
         <li className="btn btn-animated"> Member Spotlight</li>
         <li className="btn btn-animated">Our Customers</li>
-        <li className="btn btn-animated">Contact Us</li>
-        <li className="btn btn-animated" >Join Us</li>
+        <li className="btn btn-animated">Contact Us/Join Us</li>
     </ul>
 </div>
         );
