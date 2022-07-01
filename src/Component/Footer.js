28ab38b10ad6fe0612ab472783c416d05ad99b09
Siphiwe Manda <siphiwe.manda@finova.tech>
2022-07-01 12:53:32 +0100

    Update footer and Header

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -1,17 +1,20 @@
 import twitter from '../IMG/twitter.png'
 import facebook from '../IMG/facebook.png'
 import instagram from '../IMG/instagram.png'
-import '../CSS/Footer.css'
+import line from '../IMG/remove.png'
 const Footer = () => {
     return(
-        <div className="Footer">
-            <div className="FooterTop">
-                <h4>Nyanja Fruit Growers</h4>
-                <ul>
-                    <img src={facebook}  width={24} height={24} alt="facebook logo"/>
-                    <img src={instagram}  width={24} height={24} alt="instgram logo"/>
-                    <img src={twitter}  width={24} height={24} alt="twitter logo"/>
-                </ul>
+        <div className="bg-Grey">
+            <div className="flex flex-col md:flex-row">
+                <div className="flex flex-col md:flex-row items-center">
+                    <h4 className="text-center pt-5">Nyanja Fruit Growers</h4>
+                    <ul className="flex flex-row items-center pt-2 space-x-6">
+                        <img src={facebook} width={24} height={24} alt="facebook logo"/>
+                        <img src={instagram} width={24} height={24} alt="instgram logo"/>
+                        <img src={twitter} width={24} height={24} alt="twitter logo"/>
+                    </ul>
+                    <img  className="self-center" src ={line} width={50} height={50} alt="Line"/>
+                </div>
             </div>
         </div>
 
--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,20 +1,30 @@
 import mangoes from '../IMG/mangos.jpg'
 const Header = () => {
     return (
-        <div className="container flex flex-col-reverse md:flex-row items-center px-6 mx-auto mt-10 space-y-0 md:space-y-0 bg-JustPurple">
-            <div className="flex flex-col md-32 space-y-12 md:w-1/2">
-                <h1 className="max-w-md text-4xl font-bold text-center md:text-5xl md:text-left text-JustWhite"> Nyanja Fruit Growers</h1>
-                <p className="max-w-sm text-center md:text-left text-JustWhite/75"> Based in Malawi, Nyanja Fruit Growers is an association of small holder farmers, growing fruit across the country. Formed in 2020, our aim is to ............. Combining our collective knowledge and experience, we are able to support each other and other farmers across the nation by .............. Our current mission is to ................. and our long term vision is to..................
-                    We currently comprise of XX paid up members, but are open to developing and expanding our association. We are also interested in exploring investment opportunities with interested parties.
-                    Our current Board is:
-                    Chairman: Mr. Chopa Manda
-                    Deputy Chair:
-                    Treasurer:
-                    Secretary:
-                    Our association meets on a monthly basis.</p>
-            </div>
-            <div className="md:w-1/2">
-                <img src={mangoes}  alt="Fruit"/>
+        <div className="bg-JustPurple">
+            <div
+                className="container flex flex-col-reverse md:flex-row items-center px-6 mx-auto mt-1 space-y-0 md:space-y-0">
+                <div className="flex flex-col md-32 space-y-12 md:w-1/2">
+                    <h1 className="max-w-md text-4xl font-bold text-center md:text-5xl md:text-left text-JustWhite"> Nyanja
+                        Fruit Growers</h1>
+                    <p className="max-w-sm text-center md:text-left text-JustWhite/75"> Based in Malawi, Nyanja Fruit
+                        Growers is an association of small holder farmers, growing fruit across the country. Formed in
+                        2020, our aim is to ............. Combining our collective knowledge and experience, we are able
+                        to support each other and other farmers across the nation by .............. Our current mission
+                        is to ................. and our long term vision is to..................
+                        We currently comprise of XX paid up members, but are open to developing and expanding our
+                        association. We are also interested in exploring investment opportunities with interested
+                        parties.
+                        Our current Board is:
+                        Chairman: Mr. Chopa Manda
+                        Deputy Chair:
+                        Treasurer:
+                        Secretary:
+                        Our association meets on a monthly basis.</p>
+                </div>
+                <div className="md:w-1/2">
+                    <img src={mangoes} alt="Fruit"/>
+                </div>
             </div>
         </div>
     )
