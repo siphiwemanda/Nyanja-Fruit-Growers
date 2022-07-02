76c85b902b82c0c86585f29fb2802e12f05bc992
Siphiwe Manda <siphiwe.manda@finova.tech>
2022-07-02 15:34:01 +0100

    Update customer section to be a list on mobile

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -4,8 +4,8 @@ import instagram from '../IMG/instagram.png'
 import line from '../IMG/remove.png'
 const Footer = () => {
     return(
-        <div className="bg-Grey mt-10 ">
-            <div className="container mx-auto flex flex-col md:flex-row items-center px-6">
+        <div className="bg-Grey mt-20">
+            <div className="container mx-auto flex flex-col md:flex-row items-center px-6 ">
                 <div className="flex flex-col  items-center">
                     <h4 className="text-center pt-5 md:text-2xl">Nyanja Fruit Growers</h4>
                     <ul className="flex flex-row items-center pt-2 space-x-6">
--- src/Component/OurCustomers.js
+++ src/Component/OurCustomers.js
@@ -1,10 +1,10 @@
 const OurCustomers = () => {
     return (
         <div className="container flex flex-row  items-center bg-JustPurple px-6 mx-auto mt-10 space-x-6">
-            <div className="text-JustWhite md:text-6xl text-3xl p-6">
+            <div className="text-JustWhite md:text-6xl text-xl p-6">
             <h1 >Our Customers </h1>
             </div>
-            <div className="text-JustWhite p-6">
+            <div className="text-JustWhite py-6">
                 <ul>
                     <li>Malawi Mangos</li>
                     <li>Kapani</li>
