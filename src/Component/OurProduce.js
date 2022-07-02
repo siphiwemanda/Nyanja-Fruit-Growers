a877aef9c5bac0282dbf5feb32d0098d6b7584e3
Siphiwe Manda <siphiwe.manda@finova.tech>
2022-07-02 15:39:59 +0100

    Space between on produce section

--- src/Component/OurProduce.js
+++ src/Component/OurProduce.js
@@ -3,14 +3,14 @@ import fruitBox from '../IMG/organic-food.png'
 import line from '../IMG/remove.png'
 const OurProduce = ()  =>{
     return (
-        <div className="container flex flex-col items-center px-6 mx-auto space-y-0 md:space-y-0 my-10">
+        <div className="container flex flex-col items-center px-6 mx-auto space-y-5 md:space-y-0 my-10">
 
             <div className="flex flex-col items-center px-6 mx-auto mt-10 space-y-0 md:space-y-0">
                 <img  className="self-center" src ={line} width={50} height={50} alt="Line"/>
                 <h1 className="max-w-md text-4xl font-bold text-center md:text-5xl md:text-left">Our Produce</h1>
             </div>
 
-            <div className="flex flex-col md:flex-row items-center px-6 mx-auto mt-10 space-y-0 md:space-y-0">
+            <div className="flex flex-col md:flex-row items-center px-6 mx-auto mt-10 space-y-12 md:space-y-0">
             <div className="flex flex-col space-x-0.5 space-y-0.5">
                 <img className="self-center" src={fruitTree}  width={86} height={86} alt="Fruit"/>
                 <h2 className="text-center text-lg text-JustPurple"> Fresh</h2>
