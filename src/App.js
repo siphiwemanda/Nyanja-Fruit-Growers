1bc8c30682db676b9cde7385235745b45cdf4d28
Siphiwe <siphiwemanda@gmail.com>
2023-09-09 18:57:03 +0100

    Update For all onClick events

--- src/App.js
+++ src/App.js
@@ -3,35 +3,95 @@ import Navbar from "./Component/Navbar";
 import Footer from "./Component/Footer"
 import Main  from "./Component/Main";
 import ContactForm from "./Component/ContactForm";
+import AboutUsPage from "./Component/AboutUsPage";
+import OurProduce from "./Component/OurProduce";
+import MemberSpotlight from "./Component/MemberSpotlight";
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state ={
             MainPage:true,
-            Click:false,
+            AboutUsPage: false,
+            ProducePage: false,
+            MemberSpotlight: false,
+            ContactPage: false
         }
     }
-    onClick = () =>{
-        console.log('click')
-        this.setState({
-            Click:this.state.Click = true,
-            MainPage: this.state.MainPage = false
-        });
+    onClick = (e) =>{
+        console.log(e.target.id)
+        const target = e.target.id
+        if (target === "aboutUs"){
+            this.setState({
+                AboutUsPage: this.state.AboutUsPage = true,
+                ContactPage: this.state.ContactPage = false,
+                MainPage: this.state.MainPage = false,
+                MemberSpotlight: this.state.MemberSpotlight = false,
+                ProducePage: this.state.ProducePage = false
+            });
+        }
+        if (target === "contact"){
+            this.setState({
+                AboutUsPage: this.state.AboutUsPage = false,
+                ContactPage: this.state.ContactPage = true,
+                MainPage: this.state.MainPage = false,
+                MemberSpotlight: this.state.MemberSpotlight = false,
+                ProducePage: this.state.ProducePage = false
+            });
+        }
+
+        if (target === "ourProduce"){
+            this.setState({
+                AboutUsPage: this.state.AboutUsPage = false,
+                ContactPage: this.state.ContactPage = false,
+                MainPage: this.state.MainPage = false,
+                MemberSpotlight: this.state.MemberSpotlight = false,
+                ProducePage: this.state.ProducePage = true
+            });
+        }
+        if (target === "Home"){
+            this.setState({
+                AboutUsPage: this.state.AboutUsPage = false,
+                ContactPage: this.state.ContactPage = false,
+                MainPage: this.state.MainPage = true,
+                MemberSpotlight: this.state.MemberSpotlight = false,
+                ProducePage: this.state.ProducePage = false
+            });
+        }
+        if (target === "memberSpotlight"){
+            this.setState({
+                AboutUsPage: this.state.AboutUsPage = false,
+                ContactPage: this.state.ContactPage = false,
+                MainPage: this.state.MainPage = false,
+                MemberSpotlight: this.state.MemberSpotlight = true,
+                OurProduce: this.state.OurProduce = false
+            });
+        }
     }
+
     render() {
         let comp = <Main/>;
-        if (this.state.MainPage === false){
+        if (this.state.MainPage === true){
+            comp = <Main/>
+        }
+        if (this.state.AboutUsPage === true){
+            comp = <AboutUsPage/>
+        }
+        if (this.state.ContactPage === true){
             comp = <ContactForm/>
+        }
+        if (this.state.MemberSpotlight === true){
+            comp = <MemberSpotlight/>
+        }
+        if (this.state.ProducePage === true){
+            comp = <OurProduce/>
         }
         return(
         <div className="App">
-            <Navbar onClick ={this.onClick}/>
+            <Navbar onClick = {(e) => this.onClick(e)}/>
             {comp}
             <Footer />
         </div>
     )}
-
-
 }
 export default App;
